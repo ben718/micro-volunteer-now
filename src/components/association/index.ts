232092e0f534415ecd64292612c188022c8ee1d4
ben178 <benmvouama@gmail.com>
2025-06-08 10:55:41 +0200

    feat: Clear all content from repository

--- src/components/association/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as AssociationDashboard } from '../AssociationDashboard';
-export { default as MissionManagement } from '../MissionManagement';
-export { default as AssociationSettings } from '../AssociationSettings';
-export { default as AssociationMembers } from '../AssociationMembers';
-export { default as ImpactReportsPage } from '../ImpactReportsPage';
-export { default as MissionForm } from '../MissionForm'; 
--- src/pages/Index.tsx
+++
@@ -1,9 +0,0 @@
-
-import React, { useState } from 'react';
-import MobileApp from '@/components/MobileApp';
-
-const Index = () => {
-  return <MobileApp />;
-};
-
-export default Index;
